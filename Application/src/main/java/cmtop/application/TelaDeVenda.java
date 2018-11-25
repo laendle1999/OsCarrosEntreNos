4a657922826399dfb2338ed0efcfffd83d1863a4
sergiodeveloper <sergio-fh@hotmail.com>
2018-11-24 23:05:25 -0200

    Puxando alterações de Interface

--- Application/src/main/java/cmtop/application/TelaDeVenda.java
+++ Application/src/main/java/cmtop/application/TelaDeVenda.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;

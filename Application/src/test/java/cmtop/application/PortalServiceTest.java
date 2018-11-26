c3bf85851e2c857293f89bb30c4b27f56e796de3
sergiodeveloper <sergio-fh@hotmail.com>
2018-11-25 23:10:29 -0200

    Implementando PortalService

---
+++ Application/src/test/java/cmtop/application/PortalServiceTest.java
@@ -0,0 +1,19 @@
+package cmtop.application;
+
+import org.junit.Test;
+
+import cmtop.application.service.PortalService;
+
+public class PortalServiceTest {
+
+	@Test
+	public void testeAbrirPrograma() {
+		//PortalService.abrirProgramaNf();
+	}
+	
+	@Test
+	public void testeAbrirPagina() {
+		PortalService.abrirTabelaFipe();
+	}
+	
+}

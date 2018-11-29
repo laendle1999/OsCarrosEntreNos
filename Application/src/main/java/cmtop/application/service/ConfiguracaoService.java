68dfea517b8e5201a835b15e0d01d59bf1c11a31
sergiodeveloper <sergio-fh@hotmail.com>
2018-11-29 02:53:22 -0200

    Implementando importacao de backup

--- Application/src/main/java/cmtop/application/service/ConfiguracaoService.java
+++ Application/src/main/java/cmtop/application/service/ConfiguracaoService.java
@@ -7,12 +7,38 @@ import java.io.IOException;
 import org.ini4j.Wini;
 import org.zeroturnaround.zip.ZipUtil;
 
+import cmtop.application.PontoEntradaAplicacao;
+import cmtop.persistence.service.MyThread;
+
 public class ConfiguracaoService {
 
 	public static final String NOME_ARQUIVO_CONFIGURACAO = "configuracoes.ini";
 
 	public static void backupBanco(File arquivoZipSaida) {
 		ZipUtil.pack(new File("auto_manager"), arquivoZipSaida);
+	}
+
+	public static void importarBackup(File arquivoZip) {
+		new File("auto_manager").delete();
+
+		new MyThread(() -> {
+			try {
+				// Aguardar java apagar diretório antigo
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+			}
+
+			ZipUtil.unpack(arquivoZip, new File("auto_manager"));
+
+			new MyThread(() -> {
+				try {
+					// Aguardar uns instantes para a extração
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+				}
+				PontoEntradaAplicacao.finalizarAplicacao();
+			}, "ConfiguracaoService importarBackup extracao").start();
+		}, "ConfiguracaoService importarBackup").start();
 	}
 
 	public static void gravarTempoAlertaEstoque(int dias) throws IOException {
